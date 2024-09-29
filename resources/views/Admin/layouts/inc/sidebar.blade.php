228e9a3923e2aef49e2ecab9e833ad6f325028b9
AhmedTarekYA <AhmedTarekYA@github.com>
2024-09-29 20:41:19 +0300

    push

--- resources/views/Admin/layouts/inc/sidebar.blade.php
+++ resources/views/Admin/layouts/inc/sidebar.blade.php
@@ -9,7 +9,7 @@
                 <img src="{{ get_file(setting()->logo_header) }}" alt="" height="40">
             </span>
             <span class="logo-lg">
-                <img src="{{ get_file(setting()->logo_header) }}" alt="" height="70" width="110">
+                <img src="{{ get_file(setting()->logo_header) }}" alt="" height="80" width="110">
             </span>
         </a>
         <!-- Light Logo-->
@@ -18,7 +18,7 @@
                 <img src="{{ get_file(setting()->logo_header) }}" alt="" height="40">
             </span>
             <span class="logo-lg">
-                <img src="{{ get_file(setting()->logo_header) }}" alt="" height="70" width="110">
+                <img src="{{ get_file(setting()->logo_header) }}" alt="" height="80" width="110">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
